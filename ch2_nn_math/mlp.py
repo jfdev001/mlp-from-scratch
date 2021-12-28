b9d9f1efbe11447409e415755b392f0744d567ac
Jared Frazier <cscidev001@gmail.com>
2021-12-27 21:16:17 -0600

    added testing notebook and removed class: Neuron, because layer activation can be abstracted with matrix math

--- ch2_nn_math/mlp.py
+++ ch2_nn_math/mlp.py
@@ -7,47 +7,14 @@ Goodfellow et al. Deep Learning  (Ch. 6.5 pp. 200-220)
 import numpy as np
 
 
-class Neuron:
-    """A single hidden unit."""
+class DenseLayer:
+    """A densely connected layer in a neural network."""
 
-    def __init__(self, activation_function: function = None):
-        """Define state for neural network neuron.
-
-        Args:
-            activation_function: Activation function for neuron call.
-                Defaults to None (i.e., `XW + b` activation)
-        """
-
-        self.activation_function = activation_function
-
-    def activate(self, x: np.ndarray, W: np.ndarray, b: np.ndarray) -> np.float64:
-        """Activation of hidden unit.
-
-        Args:
-            x: Input vector (1 BY num_ele_in_x).
-            W: Weight matrix (num_ele_in_x BY num_hidden_units).
-            b: Bias vector (num_hidden_units). 
-
-        Returns:
-            Scalar output of activation.
-        """
-
-        # x^T @ W + b
-        geometric_transformation = np.dot(np.expand_dims(x, axis=0), W) + b
-
-        # Optional activation function
-        if self.activation_function is not None:
-            geometric_transformation = self.activation_function(
-                geometric_transformation)
-
-        # Result of activation
-        return geometric_transformation
-
-
-class Layer:
-    """A layer in the neural network."""
-
-    def __init__(self, input_dims: int, num_units: int):
+    def __init__(
+            self,
+            input_dims: int,
+            num_units: int,
+            activation_function: function = None):
         """Define state for neural network layer.
 
         Args:
@@ -55,15 +22,15 @@ class Layer:
             num_units: Number of hidden units.
         """
 
+        # Save function arg
+        self.activation_function = activation_function
+
         # Initialize weight matrix
         self.W = self.glorot_uniform(
             input_dims=input_dims, num_units=num_units)
 
         # Initialize bias vector
         self.b = np.zeros(shape=(num_units))
-
-        # Initialize list of neurons
-        self.hidden_units = [Neuron for j in num_units]
 
     def glorot_uniform(self, input_dims: int, num_units: int) -> np.ndarray:
         """(Xavier) Glorot uniform initializer.
@@ -81,6 +48,34 @@ class Layer:
         return np.random.uniform(-1/np.sqrt(input_dims), 1/np.sqrt(input_dims),
                                  size=(input_dims, num_units))
 
+    def activate(
+            self,
+            x: np.ndarray,
+            W: np.ndarray,
+            b: np.ndarray,
+            activation_function: function = None) -> np.float64:
+        """Activation of hidden units.
+
+        Args:
+            x: Input vector (1 BY num_ele_in_x).
+            W: Weight matrix (num_ele_in_x BY num_hidden_units).
+            b: Bias vector (num_hidden_units). 
+
+        Returns:
+            Scalar output of activation.
+        """
+
+        # x^T @ W + b
+        transform = np.dot(np.expand_dims(x, axis=0), W) + b
+
+        # Optional activation function
+        if activation_function is not None:
+            transform = np.apply_along_axis(
+                activation_function, axis=1, arr=transform)
+
+        # Result of activation
+        return transform
+
     def __call__(self, inputs: np.ndarray):
         """Layer computation.
 
@@ -93,7 +88,16 @@ class Layer:
         Returns:
             Vector of outputs from neurons.
         """
-        pass
+
+        # The layer call should activate the hidden units abstraction
+        layer_output = self.activate(
+            x=inputs,
+            W=self.W,
+            b=self.b,
+            activation_function=self.activation_function)
+
+        # Result of layer call
+        return layer_output
 
 
 class MLP:
