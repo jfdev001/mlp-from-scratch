b743eb77e4a980c0e06bc70ccfae40c4798d29e8
Jared Frazier <cscidev001@gmail.com>
2021-12-29 09:36:52 -0600

    moved operations to separate file

--- ch2_nn_math/mlp.py
+++ ch2_nn_math/mlp.py
@@ -15,123 +15,9 @@ Simple Backprop: https://www.youtube.com/watch?v=khUVIZ3MON8&t=20s
 Derivative of Activation Fxns: https://www.analyticsvidhya.com/blog/2021/04/activation-functions-and-their-derivatives-a-quick-complete-guide/
 """
 
-from abc import ABCMeta, abstractmethod
-from typing import Callable, Optional, Union
+from typing import Callable, Optional
 
 import numpy as np
-
-
-class Operation(metaclass=ABCMeta):
-    """Abstract class for operations with derivatives needed for backprop."""
-
-    @abstractmethod
-    def derivative(
-            self,
-            inputs: Union[tuple[np.ndarray, np.ndarray],
-                          np.ndarray]) -> np.ndarray:
-        """Call using derivative of operation.
-
-        Args:
-            inputs: Inputs for use during operation derivative call.
-                Tuple for error functions, otherwise single input.
-
-        Returns:
-            Vector of derived outputs.
-        """
-
-        pass
-
-    @abstractmethod
-    def __call__(
-            self,
-            inputs: Union[tuple[np.ndarray, np.ndarray],
-                          np.ndarray]) -> np.ndarray:
-        """Normal call of operation.
-
-        Args:
-            inputs: Inputs for use during operation call.
-                Tuple for error functions, otherwise single input.
-
-        Returns:
-            Vector of outputs.
-        """
-
-        pass
-
-
-class ReLU(Operation):
-    """ReLU activation function.
-
-    https://en.wikipedia.org/wiki/Rectifier_(neural_networks) and
-    @MISC {333400,
-        TITLE = {What is the derivative of the ReLU activation function?},
-        AUTHOR = {Jim (https://stats.stackexchange.com/users/67042/jim)},
-        HOWPUBLISHED = {Cross Validated},
-        NOTE = {URL:https://stats.stackexchange.com/q/333400 (version: 2018-03-15)},
-        EPRINT = {https://stats.stackexchange.com/q/333400},
-        URL = {https://stats.stackexchange.com/q/333400}
-    }
-    """
-
-    def derivative(self, inputs: np.ndarray) -> np.ndarray:
-
-        # Compute a boolean tensor where the elements
-        # that are greater than 0 are set to 1 while
-        # elements <= 0 are set to 0.
-        greater_than_zero_tensor = np.greater(inputs, 0).astype(np.float64)
-        return greater_than_zero_tensor
-
-    def __call__(self, inputs: np.ndarray) -> np.ndarray:
-
-        # Compute a boolean tensor where the elements
-        # that are greater than 0 are set to true while
-        # elements <= 0 are set to false.
-        greater_than_zero_tensor = np.greater(inputs, 0)
-        return greater_than_zero_tensor * inputs
-
-
-class Sigmoid(Operation):
-    """Sigmoid activation function.
-
-    @MISC {1225116,
-        TITLE = {Derivative of sigmoid function $\sigma (x) = \frac{1}{1+e^{-x}}$},
-        AUTHOR = {Michael Percy (https://math.stackexchange.com/users/229646/michael-percy)},
-        HOWPUBLISHED = {Mathematics Stack Exchange},
-        NOTE = {URL:https://math.stackexchange.com/q/1225116 (version: 2017-09-01)},
-        EPRINT = {https://math.stackexchange.com/q/1225116},
-        URL = {https://math.stackexchange.com/q/1225116}
-    }
-    """
-
-    def derivative(self, inputs: np.ndarray) -> np.ndarray:
-        return self(inputs=inputs) * (1 - self(inputs=inputs))
-
-    def __call__(self, inputs: np.ndarray) -> np.ndarray:
-        return 1 / (1 + np.exp(-inputs))
-
-
-class MeanSquaredError(Operation):
-    """Mean squared error cost (loss) function.
-
-    The predictions are the activations of the network. The order of
-    arguments in the `derivative` was based on
-    `Four fundamental equations behind backpropagation` from
-    Nielsen (Ch.2, 2015).
-    """
-
-    def derivative(
-            self,
-            inputs: tuple[np.ndarray, np.ndarray]) -> np.float64:
-
-        targets, predictions = inputs
-        return 2 * np.mean(predictions - targets)
-
-    def __call__(
-            self,
-            inputs: tuple[np.ndarray, np.ndarray]) -> np.float64:
-
-        targets, predictions = inputs
-        return np.mean(np.square(targets - predictions))
 
 
 class DenseLayer:
@@ -205,7 +91,7 @@ class DenseLayer:
 
 
 class MLP:
-    """Feedforward neural net with single hidden layer (Multilayer Perceptron)."""
+    """Feedforward neural net (Multilayer Perceptron)."""
 
     def __init__(
             self,
---
+++ ch2_nn_math/ops.py
@@ -0,0 +1,120 @@
+"""Module for handling neural network operations and their derivatives."""
+
+
+from abc import ABCMeta, abstractmethod
+from typing import Union
+
+import numpy as np
+
+
+class Operation(metaclass=ABCMeta):
+    """Abstract class for operations with derivatives needed for backprop."""
+
+    @abstractmethod
+    def derivative(
+            self,
+            inputs: Union[tuple[np.ndarray, np.ndarray],
+                          np.ndarray]) -> np.ndarray:
+        """Call using derivative of operation.
+
+        Args:
+            inputs: Inputs for use during operation derivative call.
+                Tuple for error functions, otherwise single input.
+
+        Returns:
+            Vector of derived outputs.
+        """
+
+        pass
+
+    @abstractmethod
+    def __call__(
+            self,
+            inputs: Union[tuple[np.ndarray, np.ndarray],
+                          np.ndarray]) -> np.ndarray:
+        """Normal call of operation.
+
+        Args:
+            inputs: Inputs for use during operation call.
+                Tuple for error functions, otherwise single input.
+
+        Returns:
+            Vector of outputs.
+        """
+
+        pass
+
+
+class ReLU(Operation):
+    """ReLU activation function.
+
+    https://en.wikipedia.org/wiki/Rectifier_(neural_networks) and
+    @MISC {333400,
+        TITLE = {What is the derivative of the ReLU activation function?},
+        AUTHOR = {Jim (https://stats.stackexchange.com/users/67042/jim)},
+        HOWPUBLISHED = {Cross Validated},
+        NOTE = {URL:https://stats.stackexchange.com/q/333400 (version: 2018-03-15)},
+        EPRINT = {https://stats.stackexchange.com/q/333400},
+        URL = {https://stats.stackexchange.com/q/333400}
+    }
+    """
+
+    def derivative(self, inputs: np.ndarray) -> np.ndarray:
+
+        # Compute a boolean tensor where the elements
+        # that are greater than 0 are set to 1 while
+        # elements <= 0 are set to 0.
+        greater_than_zero_tensor = np.greater(inputs, 0).astype(np.float64)
+        return greater_than_zero_tensor
+
+    def __call__(self, inputs: np.ndarray) -> np.ndarray:
+
+        # Compute a boolean tensor where the elements
+        # that are greater than 0 are set to true while
+        # elements <= 0 are set to false.
+        greater_than_zero_tensor = np.greater(inputs, 0)
+        return greater_than_zero_tensor * inputs
+
+
+class Sigmoid(Operation):
+    """Sigmoid activation function.
+
+    @MISC {1225116,
+        TITLE = {Derivative of sigmoid function $\sigma (x) = \frac{1}{1+e^{-x}}$},
+        AUTHOR = {Michael Percy (https://math.stackexchange.com/users/229646/michael-percy)},
+        HOWPUBLISHED = {Mathematics Stack Exchange},
+        NOTE = {URL:https://math.stackexchange.com/q/1225116 (version: 2017-09-01)},
+        EPRINT = {https://math.stackexchange.com/q/1225116},
+        URL = {https://math.stackexchange.com/q/1225116}
+    }
+    """
+
+    def derivative(self, inputs: np.ndarray) -> np.ndarray:
+        return self(inputs=inputs) * (1 - self(inputs=inputs))
+
+    def __call__(self, inputs: np.ndarray) -> np.ndarray:
+        return 1 / (1 + np.exp(-inputs))
+
+
+class MeanSquaredError(Operation):
+    """Mean squared error cost (loss) function.
+
+    The predictions are the activations of the network. The order of
+    arguments in the `derivative` was based on
+    `Four fundamental equations behind backpropagation` from
+    Nielsen (Ch.2, 2015).
+    """
+
+    def derivative(
+            self,
+            inputs: tuple[np.ndarray, np.ndarray]) -> np.float64:
+
+        targets, predictions = inputs
+        return 2 * np.mean(predictions - targets)
+
+    def __call__(
+            self,
+            inputs: tuple[np.ndarray, np.ndarray]) -> np.float64:
+
+        targets, predictions = inputs
+        return np.mean(np.square(targets - predictions))
